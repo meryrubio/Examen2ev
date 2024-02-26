c3ce618e82a5df25606bfe4453d2d869dd0d3ccc
meryrubio <mariarg9d@gmail.com>
2024-02-26 13:23:11 +0100

    APARTADO 3
    WitchTime

--- Examen2Eval (alumns)/Assets/Scripts/WitchTime.cs
+++ Examen2Eval (alumns)/Assets/Scripts/WitchTime.cs
@@ -4,9 +4,41 @@ using UnityEngine;
 
 public class WitchTime : MonoBehaviour
 {
+    public AudioClip wicthClip;
+    public float timeScale;
+    public float volume;
+    AudioSource audioSource;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(1))
+        {
+            AudioSource src = AudioManager.instance.PlayAudio(wicthClip, "wicthSound");
+            StartCoroutine(PlayAudio(src));
+        }
+        
+       
     }
+    IEnumerator PlayAudio(AudioSource wicthClip)
+    {
+            Time.timeScale = 0.25f;
+        while (wicthClip && wicthClip.isPlaying)
+        {
+            yield return null;
+        }
+
+        Time.timeScale = 1f;
+    }
+      
 }
+
+
+
